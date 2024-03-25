6e3b126a862ebbccb77354174cc4e51b2ea19494
Betojr04 <betomasia12@gmail.com>
2024-03-24 21:31:34 -0700

    created register route to register a new user with error handling in the event of missing information hashing the password creating jwt token and success message

--- server/api/auth.py
+++ server/api/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from .models import User, db
 import json
 import hashlib
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
@@ -16,4 +17,16 @@ def regiser_new_user():
     email = data['email']
     password = data['password']
     
+    if not name or not email or not password:
+        return jsonify({'error': 'Name, Email, and Password are requrired.'}), 400
     
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    
+    new_user = User(name=name, email=email, hashed_password=hashed_password)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    access_token = create_access_token(identity=email)
+    
+    return jsonify({'message': "User Registered Successfully", 'access_token': access_token}), 200
